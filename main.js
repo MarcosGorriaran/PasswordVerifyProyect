011f7c25a03c1d1bf3a8e63230bcbd4497e7043c
Marcos <146812640+MarcosGorriaran@users.noreply.github.com>
2024-01-18 14:00:27 +0100

    Acabado el funcionamiento basico del input
    Ahora la contraseña deberia verificar todo lo necesario y enseñar las violaciones de las constraints

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const asciiCodeForZero = 48;
+const asciiCodeForNine = 57;
+const asciiCodeForA = 65;
+const asciiCodeForZ = 90;
+const asciiCodeFora = 97;
+const asciiCodeForz = 122;
+const asciiCodeForSpace = 32;
+
 window.addEventListener('load',(event) => {
     init();
 })
@@ -18,59 +26,89 @@ function CheckPassword(inputForm){
     const MinUpperCaseReq = 1;
     const MinDigitsReq = 1;
     const RepeatedCharactersReq = 3;
+    const MaxWhiteSpaceReq = 1;
+    const MinSpecialReq = 1;
+    const InputCSSVarName = "--inputBorderColor";
+    const ErrorColor = "red";
+    const GoodColor = "green";
     const MsgBoxID = "MsgBox";
     const LengthErrorID = "NotInRange";
     const ErrorNotEnoughLowerCaseID = "NoLowerCase";
     const ErrorNotEnoughUpperCaseID = "NoUpperCase";
     const ErrorNotEnoughDigitsCaseID = "NoDigitsCase";
     const ErrorRepeatedCharactersID = "RepeatedCase";
+    const ErrorWhiteSpaceFoundID = "SpaceCase";
+    const ErrorNotEnoughSpecialCharactersID = "NoSpecialCase";
     const LengthError = "La contraseña viola la constraint de longitd de longitud minima "+MinLength+" y maxima "+MaxLength; 
     const AmountLowerCaseError = "La contraseña viola la constraint de cantidad minima de minusculas la cual es de "+MinLowerCaseReq;
     const AmountUpperCaseError = "La contraseña viola la constraint de cantidad minima de mayusculas la cual es de "+MinUpperCaseReq;
     const AmountDigitsCaseError = "La contraseña viola la constraint de cantidad minima de digitos la cual es de "+MinDigitsReq;
     const AmountRepeatedCaseError = "No pueden haver caracteres que se repita "+RepeatedCharactersReq+" veces o mas";
+    const AmountWhiteSpaceCaseError = "La contraseña viola la constraint de cantidad maxima de espacios en blanc la cual es de "+ MaxWhiteSpaceReq;
+    const AmountSpecialCharactersError = "La contraseña viola la constraint de cantidad minima de caracteres especiales la cual es de "+MinSpecialReq;
     let msgLengthError = document.getElementById(LengthErrorID);
     let msgLowerCaseError = document.getElementById(ErrorNotEnoughLowerCaseID);
     let msgUpperCaseError = document.getElementById(ErrorNotEnoughUpperCaseID);
     let msgDigitsCaseError = document.getElementById(ErrorNotEnoughDigitsCaseID);
     let msgRepeatedCharactersError = document.getElementById(ErrorRepeatedCharactersID);
-    let msgBox = document.getElementById(MsgBoxID);
+    let msgWhiteSpaceError = document.getElementById(ErrorWhiteSpaceFoundID);
+    let msgSpecialCaseError = document.getElementById(ErrorNotEnoughSpecialCharactersID);
+
+    let cssVars = document.querySelector(":root");
+    let errorCase = false;
+
     if(!InRangeLength(inputForm, MinLength, MaxLength)){
         ShowErrorMsg(LengthError, msgLengthError);
+        errorCase = true;
     }else{
         RemoveErrorMsg(msgLengthError);
     }
     if(AmountLowercases(inputForm.value)<MinLowerCaseReq){
         ShowErrorMsg(AmountLowerCaseError, msgLowerCaseError);
+        errorCase = true;
     }else{
         RemoveErrorMsg(msgLowerCaseError);
     }
     if(AmountUppercases(inputForm.value)<MinUpperCaseReq){
         ShowErrorMsg(AmountUpperCaseError, msgUpperCaseError);
+        errorCase = true;
     }else{
         RemoveErrorMsg(msgUpperCaseError);
     }
     if(AmountDigits(inputForm.value)<MinDigitsReq){
         ShowErrorMsg(AmountDigitsCaseError, msgDigitsCaseError);
+        errorCase = true;
     }else{
-        RemoveErrorMsg(msgUpperCaseError);
+        RemoveErrorMsg(msgDigitsCaseError);
     }
     if(SearchRepeatedCharacter(inputForm.value, RepeatedCharactersReq)){
         ShowErrorMsg(AmountRepeatedCaseError, msgRepeatedCharactersError);
+        errorCase = true;
     }else{
         RemoveErrorMsg(msgRepeatedCharactersError);
     }
-    if(inputForm.value.includes()){
-
+    if(AmountSpaces(inputForm.value)>=MaxWhiteSpaceReq){
+        ShowErrorMsg(AmountWhiteSpaceCaseError,msgWhiteSpaceError);
+        errorCase = true;
     }else{
-        
+        RemoveErrorMsg(msgWhiteSpaceError);
+    }
+    if(AmountSpecialCharacter(inputForm.value)<MinSpecialReq){
+        ShowErrorMsg(AmountSpecialCharactersError, msgSpecialCaseError);
+        errorCase = true;
+    }else{
+        RemoveErrorMsg(msgSpecialCaseError);
+    }
+
+    if(errorCase){
+        cssVars.style.setProperty(InputCSSVarName, ErrorColor);
+    }else{
+        cssVars.style.setProperty(InputCSSVarName, GoodColor);
     }
 
 }
 
 function ShowErrorMsg(message, documentObject){
-    
-    const MsgTag = "p";
     documentObject.innerHTML = message
 }
 function RemoveErrorMsg(documentObject){
@@ -85,8 +123,7 @@ function InRange(compareValue, minLength, maxLength){
     return compareValue>=minLength && compareValue<=maxLength;
 }
 function AmountLowercases(inputForm){
-    const asciiCodeFora = 97;
-    const asciiCodeForz = 122;
+    
     let sum = 0;
     for(let i = 0; i<inputForm.length; i++){
         let asciiCode = inputForm.charCodeAt(i);
@@ -97,8 +134,6 @@ function AmountLowercases(inputForm){
     return sum;
 }
 function AmountUppercases(inputForm){
-    const asciiCodeForA = 65;
-    const asciiCodeForZ = 90;
     let sum = 0;
     for(let i = 0; i<inputForm.length; i++){
         let asciiCode = inputForm.charCodeAt(i);
@@ -109,8 +144,6 @@ function AmountUppercases(inputForm){
     return sum;
 }
 function AmountDigits(inputForm){
-    const asciiCodeForZero = 48;
-    const asciiCodeForNine = 57;
     let sum = 0;
     for(let i = 0; i<inputForm.length; i++){
         let asciiCode = inputForm.charCodeAt(i);
@@ -120,6 +153,7 @@ function AmountDigits(inputForm){
     }
     return sum;
 }
+
 function SearchRepeatedCharacter(inputFormValue, amountRepeatTrigger){
     let sum = 1;
     let searchTarget = null;
@@ -135,4 +169,25 @@ function SearchRepeatedCharacter(inputFormValue, amountRepeatTrigger){
         }
     }
     return false;
+}
+function AmountSpaces(inputFormValue){
+    let sum = 0;
+    for(let i = 0; i<inputFormValue.length; i++){
+        let asciiCode = inputFormValue.charCodeAt(i);
+        if(asciiCode==asciiCodeForSpace){
+            sum++;
+        }
+    }
+    return sum;
+}
+function AmountSpecialCharacter(inputFormValue){
+    let sum = 0;
+    inputFormValue = inputFormValue.toUpperCase();
+    for(let i = 0; i<inputFormValue.length; i++){
+        let asciiCode = inputFormValue.charCodeAt(i);
+        if(!InRange(asciiCode ,asciiCodeForZero, asciiCodeForNine) && !InRange(asciiCode, asciiCodeForA, asciiCodeForZ) && asciiCode!=asciiCodeForSpace){
+            sum++;
+        }
+    }
+    return sum;
 }
